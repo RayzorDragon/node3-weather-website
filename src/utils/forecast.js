27e039a23fa08d1ab8725633dc17863de070c597
Raymond Gatz <Raymond.Gatz@gmail.com>
2024-01-03 13:54:00 -0500

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,25 @@
+const request = require('postman-request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=ad0f680707e1143e01ec69b74110eb20&query=' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longitude) + '&units=f'
+
+    request({ url, json: true}, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to Weather Service', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            const weatherData = {
+                current: body.current.weather_descriptions[0],
+                temperature: body.current.temperature,
+                feelsLike: body.current.feelslike
+            }
+            let data = (weatherData.current + '. It is currently ' + weatherData.temperature + ' degrees out. It feels like ' + weatherData.feelsLike + ' degrees out.')
+
+            callback(undefined, data)
+        }
+    })
+}
+
+
+module.exports = forecast
