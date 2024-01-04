1ab503be6bd2e7311d3044e8f47c1bde34701b69
Raymond Gatz <Raymond.Gatz@gmail.com>
2024-01-03 22:32:01 -0500

    Cleanup

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,13 +1,5 @@
 const request = require('postman-request')
 
-//
-// Goal: Add new data to forecast
-//
-// 1. Update the forecast string to include new data
-// 2. Commit your changes
-// 3. Push your changes to GitHub and deploy to Heroku
-// 4. Test your work in the live application
-
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=ad0f680707e1143e01ec69b74110eb20&query=' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longitude) + '&units=f'
 
@@ -23,7 +15,7 @@ const forecast = (latitude, longitude, callback) => {
                 feelsLike: body.current.feelslike,
                 humidity: body.current.humidity
             }
-            let data = (weatherData.current + '. It is currently ' + weatherData.temperature + ' degrees out. It feels like ' + weatherData.feelsLike + ' degrees out. The humidity is ' + weatherData.humidity + '.')
+            let data = (weatherData.current + '. It is currently ' + weatherData.temperature + ' degrees out. It feels like ' + weatherData.feelsLike + ' degrees out. The humidity is ' + weatherData.humidity + '%.')
 
             callback(undefined, data)
         }
